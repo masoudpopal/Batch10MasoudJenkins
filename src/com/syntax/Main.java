2e0f42c3f8b69541e505bcf2f45b4c89ee4f78d4
masoudpopal <masoudpopal123456@gmail.com>
2021-12-14 23:49:40 -0800

    Second Commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 7; i++) {
             System.out.println("Batch 10 is Great");
             Thread.sleep(2000);
 
